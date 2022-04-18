8527a28adc02de9fedbf39bd4c2b70e0c0039458
uijson1 <uijson1@gmail.com>
2022-04-18 14:04:58 +0530

    updates

--- uiJson.js
+++ uiJson.js
@@ -2,66 +2,90 @@
 @param {string} jsonStr
 */
 function renderUiJson(jsonStr, elToRenderInside) {
-  const o = JSON.parse(jsonStr)
-  if(o.type == null) {
-    throw '"type" attribute missing: jsonStr=' + jsonStr
+  const o = JSON.parse(jsonStr);
+  if (o.type == null) {
+    throw new Error('"type" attribute missing: jsonStr=' + jsonStr);
   }
-  o.type = o.type.toLowerCase()
-  s=''
-  if(o.title==null) {
-    throw '"title" is missing: jsonStr=' + jsonStr
+  o.type = o.type.toLowerCase();
+  let s = "";
+  if (o.title == null) {
+    throw new Error('"title" is missing: jsonStr=' + jsonStr);
   }
-  const name = htmlEncode(o.title)
-  s+='<h3>'+htmlEncode(o.title)+'</h3>\n'
-  if(o.description != null) {
-    s+=''+htmlEncode(o.description)+'<br><br>'
+  const name = htmlEncode(o.title);
+  s += "<h3>" + htmlEncode(o.title) + "</h3>\n";
+  if (o.description != null) {
+    s += "" + htmlEncode(o.description) + "<br><br>";
   }
-  if(o.type=='single choice'||o.type=='multiple choice') {
-    if(o.choices==null) {
-      throw '"choices" attribute is missing: jsonStr=' + jsonStr
+  if (o.type == "single choice" || o.type == "multiple choice") {
+    if (o.choices == null) {
+      throw new Error('"choices" attribute is missing: jsonStr=' + jsonStr);
     }
-    if(!Array.isArray(o.choices)) {
-      throw '"choices" attribute value is not an array: jsonStr=' + jsonStr
+    if (!Array.isArray(o.choices)) {
+      throw new Error(
+        '"choices" attribute value is not an array: jsonStr=' + jsonStr
+      );
     }
-    if(o.choices.length<2) {
-      throw '"choices" attribute value length is less than 2: jsonStr=' + jsonStr
+    if (o.choices.length < 2) {
+      throw new Error(
+        '"choices" attribute value length is less than 2: jsonStr=' + jsonStr
+      );
     }
   }
-  if(o.type=='single choice') {
-    for(let i=0;i<o.choices.length;i++) {
-      const choice = o.choices[i]
-      if(!(typeof choice == 'string')) {
-        throw '"choices" attribute value must be array of strings: jsonStr=' + jsonStr
+  if (o.type == "single choice") {
+    for (const element of o.choices) {
+      const choice = element;
+      if (typeof choice != "string") {
+        throw new Error(
+          '"choices" attribute value must be array of strings: jsonStr=' +
+            jsonStr
+        );
       }
-      const id = nextId()
-      s+='<input type="radio" name="'+name+'" id="radio'+id+'">\n'
-      s+='<label for="radio'+id+'">'+htmlEncode(choice)+'</label><br>\n'
+      const id = nextId();
+      s += '<input type="radio" name="' + name + '" id="radio' + id + '">\n';
+      s +=
+        '<label for="radio' + id + '">' + htmlEncode(choice) + "</label><br>\n";
     }
-  } else if(o.type=='multiple choice') {
-    for(let i=0;i<o.choices.length;i++) {
-      const choice = o.choices[i]
-      if(!(typeof choice == 'string')) {
-        throw '"choices" attribute value must be array of strings: jsonStr=' + jsonStr
+  } else if (o.type == "multiple choice") {
+    for (const element of o.choices) {
+      const choice = element;
+      if (typeof choice != "string") {
+        throw new Error(
+          '"choices" attribute value must be array of strings: jsonStr=' +
+            jsonStr
+        );
       }
-      const id = nextId()
-      s+='<input type="checkbox" id="checkbox'+id+'">\n'
-      s+='<label for="checkbox'+id+'">'+htmlEncode(choice)+'</label><br>\n'
+      const id = nextId();
+      s += '<input type="checkbox" id="checkbox' + id + '">\n';
+      s +=
+        '<label for="checkbox' +
+        id +
+        '">' +
+        htmlEncode(choice) +
+        "</label><br>\n";
     }
   } else {
-    throw 'Unexpected value "'+o.type+'" of "type" atrribute: jsonStr=' + jsonStr
+    throw new Error(
+      'Unexpected value "' +
+        o.type +
+        '" of "type" atrribute: jsonStr=' +
+        jsonStr
+    );
   }
-  console.log('s='+s)
-  elToRenderInside.innerHTML = s
+  console.log("s=" + s);
+  console.log('elToRenderInside==null=',elToRenderInside==null)
+  console.log("innerHTML 1=" + elToRenderInside.innerHTML);
+  elToRenderInside.innerHTML = s;
+  console.log("innerHTML 2=" + elToRenderInside.innerHTML);
 }
 
 //https://stackoverflow.com/a/29482788/15814452
-function htmlEncode (value) {
+function htmlEncode(value) {
   var p = document.createElement("p");
   p.textContent = value;
   return p.innerHTML;
 }
 
-let idCtr=0;
+let idCtr = 0;
 function nextId() {
   idCtr++;
   return idCtr;
